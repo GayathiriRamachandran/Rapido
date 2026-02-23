6d4aa61d358e59360cf67d91a544257e1a191e6f
GayathiriRamachandran <gayathiri2225@gmail.com>
2026-02-23 20:30:25 +0530

    initial commit

---
+++ program1.java
@@ -0,0 +1,18 @@
+class program1
+{
+public static void main (String[] args)
+{
+int age;
+age=16;
+System.out.println(age);
+
+char ch;
+ch='gayu';
+System.out.println(ch);
+
+double cgpa;
+cgpa=8.3;
+System.out.println(cgpa);
+}
+}
+
